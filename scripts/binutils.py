1de8b2b53ad219fd7afd435aae15e1b94f634230
ewide93 <ewide93@gmail.com>
2024-08-22 20:12:17 +0200

    Create separate directory for python scripts.

---
+++ scripts/binutils.py
@@ -0,0 +1,96 @@
+import re
+from typing import NamedTuple
+
+
+class Binutils:
+    class Symbol(NamedTuple):
+        name: str
+        address: int
+        size: int
+        symbol_type: str
+        src_file: str
+        line_num: int
+
+    COMMANDS = {
+        "size": "arm-none-eabi-size",
+        "nm": "arm-none-eabi-nm --print-size --size-sort --reverse-sort --line-numbers",
+    }
+
+    @classmethod
+    def symbol_type_to_str(cls, symbol: Symbol) -> str:
+        """Return a string representation of the given symbol type."""
+        match symbol.symbol_type:
+            case "A":
+                return "Absolute"
+            case "B" | "b":
+                return "Zero initialized data"
+            case "C" | "c":
+                return "Common"
+            case "D" | "d":
+                return "Initialized data"
+            case "G" | "g":
+                return "Small initialized data"
+            case "I" | "i":
+                return "Indirect reference"
+            case "N":
+                return "Debug"
+            case "n":
+                return "Read-only data"
+            case "p":
+                return "Stack unwind"
+            case "S" | "s":
+                return "Small zero initialized data"
+            case "T" | "t":
+                return "Code"
+            case "U":
+                return "Undefined"
+            case "u":
+                return "Unique global"
+            case "W" | "w" | "V" | "v":
+                return "Weak"
+            case "?" | _:
+                return "Unknown"
+
+    @classmethod
+    def parse_nm(cls, stdout: str) -> str:
+        """Parse output from arm-none-eabi-nm utility."""
+        symbols = []
+        for line in stdout.removesuffix("\n").split("\n"):
+            address, size, symbol_type, name, path_and_line_num = line.split()
+            _, file_path, line_num = path_and_line_num.split(":")
+            regex = re.compile(r"^.+\/([a-zA-Z_]+.c)$")
+            match = regex.match(file_path)
+            if match:
+                src_file = match.group(1)
+                symbols.append(
+                    cls.Symbol(
+                        name,
+                        hex(int(address, 16)),
+                        int(size, 16),
+                        symbol_type,
+                        src_file,
+                        int(line_num),
+                    )
+                )
+        src_files = []
+        parsed_output = ""
+        for symbol in sorted(symbols, key=lambda sym: sym.src_file):
+            if symbol.src_file not in src_files:
+                src_files.append(symbol.src_file)
+                parsed_output += (
+                    "\n" + f"-- Source file: {symbol.src_file} ".ljust(88, "-") + "\n"
+                )
+            parsed_output += f"Name: {symbol.name}\n"
+            parsed_output += f"\tAddress: {symbol.address}".ljust(22, " ")
+            parsed_output += f"Size: {symbol.size}".ljust(10, " ")
+            parsed_output += f"Line: {symbol.line_num}".ljust(10, " ")
+            parsed_output += f"Type: {cls.symbol_type_to_str(symbol)}\n"
+        return parsed_output
+
+    @staticmethod
+    def parse_size(stdout: str) -> tuple:
+        """Parse output from arm-none-eabi-size utility returning
+        a tuple containing the sizes of the text,
+        data & bss sections in bytes."""
+        text, data, bss = stdout.split("\n")[1].split()[:3]
+        return (int(text), int(data), int(bss))
--- tasks.py
+++ tasks.py
@@ -1,19 +1,7 @@
-import sys
-import re
 from invoke import task
 from invoke.context import Context
 from pathlib import Path
-from typing import NamedTuple, List
-from enum import IntEnum
-
-
-class Symbol(NamedTuple):
-    name: str
-    address: int
-    size: int
-    symbol_type: str
-    src_file: str
-    line_num: int
+from scripts.binutils import Binutils
 
 
 PATHS = {
@@ -31,78 +19,6 @@ DEVICE_INFO = {
     "ram": {"bytes": 131072, "KiB": 128},
 }
 
-BINUTILS = {
-    "size": "arm-none-eabi-size",
-    "nm": "arm-none-eabi-nm",
-}
-
-
-def parse_size(stdout: str) -> tuple:
-    """Parse output from arm-none-eabi-size utility returning
-    a tuple containing the sizes of the text,
-    data & bss sections in bytes."""
-    text, data, bss = stdout.split("\n")[1].split()[:3]
-    return (int(text), int(data), int(bss))
-
-
-def parse_nm(stdout: str) -> List[Symbol]:
-    """Parse output from arm-none-eabi-nm utility returning
-    a list of tuples containing symbol address,
-    symbol size, symbol type, symbol name, source file for symbol
-    and the line number in the source file where the symbol is defined."""
-    symbols = []
-    for line in stdout.removesuffix("\n").split("\n"):
-        address, size, symbol_type, name, path_and_line_num = line.split()
-        _, file_path, line_num = path_and_line_num.split(":")
-        regex = re.compile(r"^.+\/([a-zA-Z_]+.c)$")
-        match = regex.match(file_path)
-        if match:
-            src_file = match.group(1)
-            symbols.append(
-                Symbol(
-                    name,
-                    hex(int(address, 16)),
-                    int(size, 16),
-                    symbol_type,
-                    src_file,
-                    int(line_num),
-                )
-            )
-    return sorted(symbols, key=lambda sym: sym.src_file)
-
-def symbol_type_to_str(symbol: Symbol) -> str:
-    """Return a string representation of the given symbol type."""
-    match symbol.symbol_type:
-        case "A":
-            return "Absolute"
-        case "B" | "b":
-            return "Zero initialized data"
-        case "C" | "c":
-            return "Common"
-        case "D" | "d":
-            return "Initialized data"
-        case "G" | "g":
-            return "Small initialized data"
-        case "I" | "i":
-            return "Indirect reference"
-        case "N":
-            return "Debug"
-        case "n":
-            return "Read-only data"
-        case "p":
-            return "Stack unwind"
-        case "S" | "s":
-            return "Small zero initialized data"
-        case "T" | "t":
-            return "Code"
-        case "U":
-            return "Undefined"
-        case "u":
-            return "Unique global"
-        case "W" | "w" | "V" | "v":
-            return "Weak"
-        case "?" | _:
-            return "Unknown"
 
 @task()
 def build(ctx: Context) -> None:
@@ -130,8 +46,8 @@ def size(ctx: Context) -> None:
     """Print the firmware image size."""
     if not PATHS["elf"].exists():
         print(f"Path {PATHS['elf']} does not exist.")
-    output = ctx.run(f"{BINUTILS['size']} {PATHS['elf']}", hide=True)
-    text, data, bss = parse_size(output.stdout)
+    output = ctx.run(f"{Binutils.COMMANDS["size"]} {PATHS['elf']}", hide=True)
+    text, data, bss = Binutils.parse_size(output.stdout)
     flash_used = text + data
     flash_used_percent = round(flash_used / DEVICE_INFO["flash"]["bytes"] * 100, 2)
     ram_used = data + bss
@@ -143,16 +59,5 @@ def size(ctx: Context) -> None:
 @task()
 def nm(ctx: Context) -> None:
     """List the symbols in the firmware image."""
-    flags = "--print-size --size-sort --reverse-sort --line-numbers"
-    output = ctx.run(f"{BINUTILS['nm']} {flags} {PATHS['elf']}", hide=True)
-    symbols = parse_nm(output.stdout)
-    src_files = []
-    for symbol in symbols:
-        if symbol.src_file not in src_files:
-            src_files.append(symbol.src_file)
-            print("\n" + f"-- Source file: {symbol.src_file} ".ljust(88, "-"))
-        print(f"Name: {symbol.name}")
-        print(f"Address: {symbol.address}".ljust(22, " "), end="")
-        print(f"Size: {symbol.size}".ljust(10, " "), end="")
-        print(f"Line: {symbol.line_num}".ljust(10, " "), end="")
-        print(f"Type: {symbol_type_to_str(symbol)}\n")
+    output = ctx.run(f"{Binutils.COMMANDS["nm"]} {PATHS['elf']}", hide=True)
+    print(Binutils.parse_nm(output.stdout))
