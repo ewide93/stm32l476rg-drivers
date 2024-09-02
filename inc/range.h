99f5179cd77091ca51230989dedf39d479bcfb29
ewide93 <ewide93@gmail.com>
2024-09-02 20:21:14 +0200

    Add range checking module

---
+++ inc/range.h
@@ -0,0 +1,59 @@
+/**
+ * @file range.h
+ *
+ * @brief Range checking functions.
+ */
+
+#ifndef RANGE_H
+#define RANGE_H
+
+/* ------------------------------- Include directives ------------------------------ */
+#include "typedef.h"
+
+/* -------------------------- Public function definitions -------------------------- */
+static inline bool Range_U8(U8 Value, U8 Min, U8 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_U16(U16 Value, U16 Min, U16 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_U32(U32 Value, U32 Min, U32 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_U64(U64 Value, U64 Min, U64 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_S8(S8 Value, S8 Min, S8 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_S16(S16 Value, S16 Min, S16 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_S32(S32 Value, S32 Min, S32 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_S64(S64 Value, S64 Min, S64 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+static inline bool Range_F32(F32 Value, F32 Min, F32 Max)
+{
+    return (Value >= Min && Value <= Max);
+}
+
+#endif  /* RANGE_H*/
