f902545b1494a39a0a002b60cec4c30dbea794b6
ewide93 <ewide93@gmail.com>
2026-01-17 20:51:39 +0100

    Rotary encoder stuff

--- stm32l476rg/app/main.c
+++ stm32l476rg/app/main.c
@@ -6,6 +6,7 @@
  */
 
 /* ------------------------------- Include directives ------------------------------ */
+
 #include "typedef.h"
 #include "stm32l4xx.h"
 #include "systick.h"
@@ -20,6 +21,71 @@
 #include "mempool.h"
 #include "osal.h"
 #include "core_debug.h"
+#include "limit.h"
+
+/* ----------------------------------- Move this! ---------------------------------- */
+
+typedef struct
+{
+    Digital_InputType PinA;
+    Digital_InputType PinB;
+    Bool CurrentA;
+    Bool PreviousA;
+    Bool CurrentB;
+    Bool Invert;
+} RotEnc_SimpleRotEncType;
+
+typedef enum
+{
+    ENCODER_EVENT_NONE = 0x00U,
+    ENCODER_EVENT_CLOCKWISE = 0x01U,
+    ENCODER_EVENT_ANTI_CLOCKWISE = 0x02U,
+    ENCODER_EVENT_ENUM_LIMIT
+} RotEnc_EventEnum;
+
+void RotEnc_Init(RotEnc_SimpleRotEncType* RotEnc, Pin_PortPinEnum PinA, Pin_PortPinEnum PinB, Bool Invert)
+{
+    /* Initialize digital inputs */
+    RotEnc->PinA.PortPin = PinA;
+    RotEnc->PinA.Resistor = PIN_RES_NONE;
+    RotEnc->PinB.PortPin = PinB;
+    RotEnc->PinB.Resistor = PIN_RES_NONE;
+    Digital_InputInit(&RotEnc->PinA);
+    Digital_InputInit(&RotEnc->PinB);
+
+    /* Read initial state of pins */
+    RotEnc->CurrentA = Digital_Read(&RotEnc->PinA);
+    RotEnc->PreviousA = RotEnc->CurrentA;
+    RotEnc->CurrentB = Digital_Read(&RotEnc->PinB);
+
+    /* Set rotation polarity */
+    RotEnc->Invert = Invert;
+}
+
+RotEnc_EventEnum RotEnc_Poll(RotEnc_SimpleRotEncType* RotEnc)
+{
+    RotEnc_EventEnum Event = ENCODER_EVENT_NONE;
+    RotEnc->PreviousA = RotEnc->CurrentA;
+    RotEnc->CurrentA = Digital_Read(&RotEnc->PinA);
+    RotEnc->CurrentB = Digital_Read(&RotEnc->PinB);
+
+    if (RotEnc->CurrentA != RotEnc->PreviousA)
+    {
+        if (RotEnc->CurrentA) /* Rising edge */
+        {
+            if (!RotEnc->CurrentB) { Event = RotEnc->Invert ? ENCODER_EVENT_ANTI_CLOCKWISE : ENCODER_EVENT_CLOCKWISE; }
+            else                   { Event = RotEnc->Invert ? ENCODER_EVENT_CLOCKWISE : ENCODER_EVENT_ANTI_CLOCKWISE; }
+        }
+        else /* Falling edge */
+        {
+            if (RotEnc->CurrentB) { Event = RotEnc->Invert ? ENCODER_EVENT_ANTI_CLOCKWISE : ENCODER_EVENT_CLOCKWISE; }
+            else                  { Event = RotEnc->Invert ? ENCODER_EVENT_CLOCKWISE : ENCODER_EVENT_ANTI_CLOCKWISE; }
+        }
+    }
+
+    return Event;
+}
+
 
 /* -------------------------------- Global variables ------------------------------- */
 
@@ -30,7 +96,8 @@ Digital_OutputType OutputA5 =
     .Speed = PIN_SPEED_LOW,
     .InitVal = DIGITAL_STATE_HIGH
 };
-static volatile U32 DummyCnt = 10UL;
+
+RotEnc_SimpleRotEncType Encoder = { 0 };
 
 /* -------------------------- Local function declarations -------------------------- */
 
@@ -55,6 +122,7 @@ int main(void)
     Protocol_Init(USART2, 115200, PIN_A2, PIN_A3);
 
     Digital_OutputInit(&OutputA5);
+    RotEnc_Init(&Encoder, PIN_A0, PIN_A1, True);
 
     Osal_ThreadCreate(BlinkThreadFunc, NULL, 1024, THREAD_PRIORITY_MEDIUM);
     Osal_ThreadCreate(CommThreadFunc, NULL, 1024, THREAD_PRIORITY_MEDIUM);
@@ -120,20 +188,38 @@ void Setup(void)
 void BlinkThreadFunc(void* Arg)
 {
     UNUSED(Arg);
-    U32 Period_Ticks = Osal_msToTicks(10UL);
+    U32 Period_Ticks = Osal_msToTicks(2UL);
     U32 Timekeeping = Osal_GetTickCount();
-    U8 Cnt = 0U;
-    U8 ToggleCnt = 0U;
+    U16 Cnt = 0U;
+    U16 Threshold = 250U;
+
     while (True)
     {
-        if (++Cnt > 50)
+        /* Poll rotary encoder for event */
+        RotEnc_EventEnum EncoderEvent = RotEnc_Poll(&Encoder);
+        if (EncoderEvent == ENCODER_EVENT_CLOCKWISE)
+        {
+            Threshold += 5;
+            Threshold = Limit_U16(Threshold, 25, 500);
+        }
+        else if (EncoderEvent == ENCODER_EVENT_ANTI_CLOCKWISE)
+        {
+            Threshold -= 5;
+            Threshold = Limit_U16(Threshold, 25, 500);
+        }
+        else
+        {
+            __NOP();
+        }
+
+        /* Toggle LED */
+        if (++Cnt >= Threshold)
         {
             Digital_Toggle(&OutputA5);
             Cnt = 0U;
-            ToggleCnt++;
-            if (ToggleCnt >= 10U) { __BKPT(0); }
         }
-        DummyCnt++;
+
+        /* Put thread to sleep until next iteration */
         Osal_DelayUntil(&Timekeeping, Period_Ticks);
     }
 }
